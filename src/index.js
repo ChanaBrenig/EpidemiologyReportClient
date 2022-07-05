22347592386c8f519995dbb8c79041aa7b104dd4
ChanaBrenig <shchana63@gmail.com>
2022-07-05 12:24:44 +0300

    add promise function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 ﻿// const helloTitle = document.createElement('h2');
 // helloTitle.innerText = 'Hello';
 // document.body.appendChild(helloTitle);
+
+new Promise(function (resolve, reject) {
+
+    setTimeout(() => { resolve(Math.ceil(Math.random() * 10)) }, 3000)
+}).then(
+    result => alert(result));
+
+

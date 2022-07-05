a5611b6a9bc8b25ae7306bd36293105ef878445e
ChanaBrenig <shchana63@gmail.com>
2022-07-05 12:35:52 +0300

    delete comments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
-﻿// const helloTitle = document.createElement('h2');
-// helloTitle.innerText = 'Hello';
-// document.body.appendChild(helloTitle);
-
-new Promise(function (resolve, reject) {
+﻿new Promise(function (resolve, reject) {
 
     setTimeout(() => { resolve(Math.ceil(Math.random() * 10)) }, 3000)
 }).then(

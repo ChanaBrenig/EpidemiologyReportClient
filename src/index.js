a76b675583a228474928761f26d767ba39b48697
NechamiAndMiri <97882934+NechamiAndMiri@users.noreply.github.com>
2022-07-05 11:50:52 +0300

    miris work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-﻿// const helloTitle = document.createElement('h2');
-// helloTitle.innerText = 'Hello';
-// document.body.appendChild(helloTitle);
+﻿
+const arr=['Chana','Miri','Shani','Ricky','Shira']
+
+const makeAllCaps=(wordsArr)=>{
+    return wordsArr.map((word)=>word.toUpperCase());
+}
+
+const sortWords=(wordsArr)=>{
+    return wordsArr.sort();
+}
+
+ new Promise((resolve, reject) => { 
+     if(arr.length==0||arr.every((word)=>typeof word === String))
+        reject(new Error("Invalid wordsArr"));
+    resolve(makeAllCaps(arr));
+  
+}).then((res)=>sortWords(res)).catch((err) => alert(err.message)).then((res)=>console.log(res));
+
+
